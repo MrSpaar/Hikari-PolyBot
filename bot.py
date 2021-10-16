c9f3d318c803f3b22bbbf66b6da4967abf1b80e8
MrSpaar <julienpistre.cagnes@gmail.com>
2021-10-17 00:24:22 +0200

    Minor improvement

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from core.cls import Bot
 
 bot = Bot(debug=False)
 
-for plugin in (path.replace('/', '.')[:-3] for path in glob('plugins/*.py')):
+for plugin in glob('plugins/*.py'):
+    plugin = plugin.replace('/', '.')[:-3]
     bot.load_extension(plugin)
 
 bot.run()
