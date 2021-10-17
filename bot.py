c1a64f6247166434deb73cd09184d1e8fbbc1cd1
MrSpaar <julienpistre.cagnes@gmail.com>
2021-10-17 20:21:32 +0200

    Added a status to the bot

--- bot.py
+++ bot.py
@@ -1,7 +1,13 @@
+from hikari import StartedEvent, Activity
+
 from glob import glob
 from core.cls import Bot
 
 bot = Bot(debug=False)
+
+@bot.listen(StartedEvent)
+async def set_status(_):
+    await bot.update_presence(activity=Activity(type=0, name=f'{bot.cprefix}help'))
 
 for plugin in glob('plugins/*.py'):
     plugin = plugin.replace('/', '.')[:-3]
