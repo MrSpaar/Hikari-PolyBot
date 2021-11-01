c390ace9b821d22cb0bbeafbf7872d8c86778354
MrSpaar <julienpistre.cagnes@gmail.com>
2021-11-01 15:56:43 +0100

    Added linux support

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 from hikari import StartedEvent, Activity
 
 from glob import glob
+from sys import platform
 from core.cls import Bot
 
 bot = Bot(debug=False)
@@ -9,8 +10,11 @@ bot = Bot(debug=False)
 async def set_status(_):
     await bot.update_presence(activity=Activity(type=0, name=f'{bot.cprefix}help'))
 
+sep = '/' if platform == 'linux' else '\\'
+
 for plugin in glob('plugins/*.py'):
-    plugin = plugin.replace('/', '.')[:-3]
+    plugin = plugin.replace(sep, '.')[:-3]
     bot.load_extension(plugin)
+
 
 bot.run()
