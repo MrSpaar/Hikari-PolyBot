62910e8a341c4deba0094baf4df3d0042366d9d7
MrSpaar <julienpistre.cagnes@gmail.com>
2022-01-14 19:31:07 +0000

    plugins/music.py
    Improved music plugin

--- plugins/music.py
+++ plugins/music.py
@@ -1,4 +1,4 @@
-from hikari import Embed, ShardReadyEvent
+from hikari import Embed, Message, ShardReadyEvent, GuildReactionAddEvent, MessageFlag
 from lavasnek_rs import Lavalink, LavalinkBuilder, Track
 from lightbulb import (
     Plugin,
@@ -12,24 +12,35 @@ from lightbulb import (
 )
 
 
+class EventHandler:
+    async def track_finish(self, lavalink: Lavalink, event):
+        await update_queue(lavalink, event.guild_id)
+
+
+async def create_message(ctx: Context, track) -> Message:
+    embed1 = Embed(color=0x3498DB, description=f"🎵 [`{track.info.title}`]({track.info.uri}) de `{track.info.author}`\n🙍 Demandé par {ctx.author.mention}")
+    embed2 = Embed(color=0x99AAB5, description="*Pas de vidéos en attente*")
+
+    resp = await ctx.respond(embeds=[embed1, embed2])
+    message = await resp.message()
+
+    for emoji in ("⏹️", "⏯️", "⏭️"):
+        await message.add_reaction(emoji)
+
+    return message
+
+
 async def update_queue(lavalink: Lavalink, guild_id: int, track: Track = None):
     node = await lavalink.get_guild_node(guild_id)
+
+    if not node.now_playing and not node.queue:
+        return await stop(lavalink, guild_id)
+
     message = await node.get_data()
-    np = node.now_playing
-
-    if not np:
-        await _stop(lavalink, guild_id)
-
-        try:
-            await message.delete()
-        except:
-            pass
-
-        return
-
     embeds = message.embeds
+    np = node.queue[0]
 
-    tracks = [t.track.info for t in node.queue[1:]] + ([track.info] if track else [])
+    tracks = [t.track.info for t in node.queue[1:]] + [track.info] if track else []
     queue = [
         f"{i+1}) [`{info.title}`]({info.uri}) de `{info.author}`"
         for i, info in enumerate(tracks)
@@ -41,20 +52,32 @@ async def update_queue(lavalink: Lavalink, guild_id: int, track: Track = None):
     await message.edit(embeds=embeds)
 
 
-class EventHandler:
-    async def track_finish(self, lavalink: Lavalink, event):
-        await update_queue(lavalink, event.guild_id)
+async def stop(lavalink, guild_id):
+    node = await lavalink.get_guild_node(guild_id)
+    try: await (await node.get_data()).delete()
+    except: pass
 
-    async def track_exception(self, lavalink: Lavalink, event):
-        skip = await lavalink.skip(event.guild_id)
-        node = await lavalink.get_guild_node(event.guild_id)
+    await lavalink.destroy(guild_id)
+    await lavalink.leave(guild_id)
+    await lavalink.remove_guild_node(guild_id)
+    await lavalink.remove_guild_from_loops(guild_id)
 
-        if not skip:
-            embed = Embed(color=0xE74C3C, description="❌ Aucune vidéo à skip")
-            await event.message.respond(embed=embed)
-        else:
-            if not node.queue and not node.now_playing:
-                await _stop(event.guild_id)
+
+async def join(ctx):
+    states = ctx.bot.cache.get_voice_states_view_for_guild(ctx.get_guild())
+    voice_state = list(filter(lambda i: i.user_id == ctx.author.id, states.iterator()))
+
+    if not voice_state:
+        embed = Embed(color=0xE74C3C, description="❌ Tu n'es connecté à aucun salon vocal")
+        return await ctx.respond(embed=embed, flags=MessageFlag.EPHEMERAL)
+
+    try:
+        connection_info = await ctx.bot.data.lavalink.join(ctx.guild_id, voice_state[0].channel_id)
+    except TimeoutError:
+        embed = Embed(color=0xE74C3C, description="❌ Je n'ai pas pu rejoindre le salon")
+        return await ctx.respond(embed=embed, flags=MessageFlag.EPHEMERAL)
+
+    await ctx.bot.data.lavalink.create_session(connection_info)
 
 
 plugin = Plugin("Musique")
@@ -76,40 +99,15 @@ async def start_lavalink(_):
         plugin.bot.remove_plugin(plugin)
 
 
-async def _stop(lavalink, guild_id):
-    await lavalink.destroy(guild_id)
-
-    await lavalink.leave(guild_id)
-    await lavalink.remove_guild_node(guild_id)
-    await lavalink.remove_guild_from_loops(guild_id)
-
-
-async def _join(ctx):
-    states = ctx.bot.cache.get_voice_states_view_for_guild(ctx.get_guild())
-    voice_state = list(filter(lambda i: i.user_id == ctx.author.id, states.iterator()))
-
-    if not voice_state:
-        embed = Embed(color=0xE74C3C, description="❌ Tu n'es connecté à aucun salon vocal")
-        return await ctx.respond(embed=embed)
-
-    try:
-        connection_info = await ctx.bot.data.lavalink.join(
-            ctx.guild_id, voice_state[0].channel_id
-        )
-    except TimeoutError:
-        embed = Embed(color=0xE74C3C, description="❌ Je n'ai pas pu rejoindre le salon")
-        return await ctx.respond(embed=embed)
-
-    await ctx.bot.data.lavalink.create_session(connection_info)
-
-
 @plugin.command()
 @option("texte", "Le titre de la vidéo", modifier=OptionModifier.CONSUME_REST)
 @command("play", "Écouter une vidéo dans le channel où vous êtes connecté")
 @implements(SlashCommand)
 async def play(ctx: Context):
     if not await ctx.bot.data.lavalink.get_guild_gateway_connection_info(ctx.guild_id):
-        await _join(ctx)
+        failed = await join(ctx)
+        if failed:
+            return
 
     query = await ctx.bot.data.lavalink.auto_search_tracks(ctx.options.texte)
 
@@ -122,51 +120,37 @@ async def play(ctx: Context):
 
     if node.now_playing:
         await update_queue(ctx.bot.data.lavalink, ctx.guild_id, track)
+        await ctx.respond("Musique ajoutée", flags=MessageFlag.EPHEMERAL)
     else:
-        embed1 = Embed(color=0x3498DB, description=f"🎵 [`{track.info.title}`]({track.info.uri}) de `{track.info.author}`\n🙍 Demandé par {ctx.author.mention}",)
-        embed2 = Embed(color=0x99AAB5, description="*Pas de vidéos en attente*")
-
-        resp = await ctx.respond(embeds=[embed1, embed2])
-        message = await resp.message()
+        message = await create_message(ctx, track)
         await node.set_data(message)
 
     await ctx.bot.data.lavalink.play(ctx.guild_id, track).requester(ctx.author.id).queue()
 
 
-@plugin.command()
-@command("leave", "Déconnecter le bot du channel")
-@implements(SlashCommand)
-async def leave(ctx: Context):
-    await _stop(ctx.bot.data.lavalink, ctx.guild_id)
+@plugin.listener(GuildReactionAddEvent)
+async def reaction_pressed(event):
+    if event.member.is_bot:
+        return
 
+    node = await plugin.bot.data.lavalink.get_guild_node(event.guild_id)
+    message = await node.get_data()
 
-@plugin.command()
-@command("skip", "Passer la vidéo en cours de lecture")
-@implements(SlashCommand)
-async def skip(ctx: Context):
-    skip = await ctx.bot.data.lavalink.skip(ctx.guild_id)
-    node = await ctx.bot.data.lavalink.get_guild_node(ctx.guild_id)
+    await message.remove_reaction(event.emoji_name, user=event.member.id)
 
-    if not skip:
-        embed = Embed(color=0xE74C3C, description="❌ Aucune vidéo en cours de lecture")
-        return await ctx.respond(embed=embed)
+    if not message:
+        return
 
-    if not node.queue and not node.now_playing:
-        await _stop(ctx.bot.data.lavalink, ctx.guild_id)
-
-
-@plugin.command()
-@command("pause", "Mettre en pause la vidéo en cours de lecture")
-@implements(SlashCommand)
-async def pause(ctx):
-    await ctx.bot.data.lavalink.pause(ctx.guild_id)
-
-
-@plugin.command()
-@command("resume", "Reprendre la vidéo en cours de lecture")
-@implements(SlashCommand)
-async def resume(ctx):
-    await ctx.bot.data.lavalink.resume(ctx.guild_id)
+    if event.emoji_name == "⏹️":
+        await stop(plugin.bot.data.lavalink, event.guild_id)
+    elif event.emoji_name == "⏭️":
+        await plugin.bot.data.lavalink.skip(event.guild_id)
+    elif event.emoji_name == "⏭️" and not node.queue and not node.now_playing:
+        await stop(plugin.bot.data.lavalink, event.guild_id)
+    elif event.emoji_name == "⏯️" and not node.is_paused:
+        await plugin.bot.data.lavalink.pause(event.guild_id)
+    else:
+        await plugin.bot.data.lavalink.resume(event.guild_id)
 
 
 def load(bot):
--- src/bot.py
+++ src/bot.py
@@ -10,10 +10,13 @@ from dotenv import load_dotenv
 class Bot(BotApp):
     def __init__(self, debug: bool = False):
         load_dotenv()
+
+        guilds = (930077129411018793,) if debug else (752921557214429316, 634339847108165632, 339045627478540288, 930077129411018793)
+
         super().__init__(
             intents=Intents.ALL,
             token=environ["DEBUG_TOKEN"] if debug else environ["BOT_TOKEN"],
-            default_enabled_guilds=(930077129411018793,) if debug else (752921557214429316, 634339847108165632, 339045627478540288),
+            default_enabled_guilds=guilds,
             logs="ERROR",
         )
 
