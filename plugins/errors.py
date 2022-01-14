072e2c175289e2980d9b8927188a6b346322d615
MrSpaar <julienpistre.cagnes@gmail.com>
2022-01-14 19:27:23 +0000

    Removed unnessary errors

--- plugins/errors.py
+++ plugins/errors.py
@@ -1,58 +1,27 @@
-from hikari import Embed
-from lightbulb import Plugin, CommandErrorEvent, errors
+from hikari import Embed, MessageFlag
+from lightbulb import Plugin, SlashCommandErrorEvent, errors
 
 plugin = Plugin("Erreurs")
 
 
-@plugin.listener(CommandErrorEvent)
+@plugin.listener(SlashCommandErrorEvent)
 async def on_command_error(event):
-    ctx, error = event.context, event.exception
+    ctx, error = event.context, event.exception.__cause__
 
     handled = {
         errors.MissingRequiredPermission: "❌ Tu n'as pas la permission de faire ça",
         errors.MissingRequiredRole: "❌ Tu n'as pas la permission de faire ça",
         errors.BotMissingRequiredPermission: "❌ Je n'ai pas la permission de faire ça",
-        errors.NotEnoughArguments: f"❌ Il manque des arguments : `{', '.join(getattr(error, 'missing_args', ''))}`",
-        errors.CheckFailure: "❌ Tu n'es pas le créateur de ce channel ou tu n'es pas connecté à un channel",
-        errors.CommandIsOnCooldown: "❌ Commande en cooldown",
+        errors.CheckFailure: "❌ Tu n'as pas la permission de faire ça",
         errors.OnlyInGuild: "❌ Cette commande n'est utilisable que sur un serveur",
         errors.NotOwner: "❌ Seul le créateur du bot peut utiliser cette commande",
-        errors.ConverterFailure: {
-            "member": "❌ Membre inexistant",
-            "emoji": "❌ Cette commande ne marche qu'avec les emojis custom",
-            "channel": "❌ Channel introuvable",
-            "int": "❌ Les arguments doivent être des nombres entiers",
-        },
-        errors.CommandInvocationError: {
-            "Not temp": "❌ Tu n'es pas dans un channel temporaire",
-            "Not owner": "❌ Tu n'es pas le créateur de ce channel",
-            "channel": "❌ Tu n'es connecté à aucun channel",
-            "string index": "❌ Erreur dans la conversion",
-            "list index": "❌ Recherche invalide, aucun résultat trouvé",
-            "UnknownObjectException": "❌ Recherche invalide, aucun résultat trouvé",
-            "not enough values to unpack": "❌ Lancer invalide",
-            "KeyError: 'list'": "❌ Ville introuvable ou inexistante",
-            "This video may be": "❌ Restriction d'âge, impossible de jouer la vidéo",
-            "No video formats found": "❌ Aucun format vidéo trouvé, impossible de jouer la vidéo",
-            "RecursionError": "❌ Trop de récursions, nombre trop grand",
-            "4000": "❌ Mon message est trop long, impossible de l'envoyer",
-        },
     }
 
     if type(error) not in handled:
         raise error
 
-    error_entry = handled[type(error)]
-    if isinstance(error_entry, dict):
-        for key, value in error_entry.items():
-            if key in str(error):
-                error_entry = value
-
-    if isinstance(error_entry, dict):
-        raise error
-
-    embed = Embed(color=0xE74C3C, description=error_entry)
-    await ctx.respond(embed=embed)
+    embed = Embed(color=0xE74C3C, description=handled[type(error)])
+    await ctx.respond(embed=embed, flags=MessageFlag.EPHEMERAL)
 
 
 def load(bot):
