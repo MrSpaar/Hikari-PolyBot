886cf855b8ac8037902c72e8e29a013020f1cb50
MrSpaar <julienpistre.cagnes@gmail.com>
2022-01-15 19:16:21 +0000

    Minor change

--- main.py
+++ main.py
@@ -2,13 +2,19 @@ from hikari import StartedEvent, Activity
 
 from src.bot import Bot
 
-bot = Bot()
+
+def main():
+    bot = Bot()
 
 
-@bot.listen(StartedEvent)
-async def ready(_):
-    print("Bot is ready")
+    @bot.listen(StartedEvent)
+    async def ready(_):
+        print("Bot is ready")
 
 
-bot.load_extensions_from("./plugins/")
-bot.run(activity=Activity(name='vous observer', type=0))
+    bot.load_extensions_from("./plugins/")
+    bot.run(activity=Activity(name='vous observer', type=0))
+
+
+if __name__ == "__main__":
+    main()
