9ef54d7164d9000266365d9200c78fec5ef8509a
MrSpaar <julienpistre.cagnes@gmail.com>
2022-06-30 12:01:35 +0200

    global commands

--- core/bot.py
+++ core/bot.py
@@ -7,12 +7,9 @@ from os import environ
 
 class ExtendedBot(BotApp, ABC):
     def __init__(self):
-        guilds = (752921557214429316, 634339847108165632, 339045627478540288)
-
         super().__init__(
             intents=Intents.ALL,
             token=environ["BOT_TOKEN"],
-            default_enabled_guilds=guilds,
             logs="ERROR",
         )
 
